26b30668fbc423af7fbe65b433be74b696a08856
Dmitry <bright1308@gmail.com>
2017-03-12 11:41:17 +0700

    Коммит СFT10032017

--- CFT10032017/src/ru/academits/schepin/insertionSort/Main.java
+++ CFT10032017/src/ru/academits/schepin/insertionSort/Main.java
@@ -10,7 +10,7 @@ import static ru.academits.schepin.insertionSort.Sort.sortAscending;
 import static ru.academits.schepin.insertionSort.Sort.sortDecreasing;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         //todo Проверка кол-ва переданных аргументов:
         if (args.length != 4) {
             System.out.println("Недостаточно переданных параметров");
@@ -23,7 +23,7 @@ public class Main {
         String ascendingOrDecrease = args[2];
         String dataType = args[3];
 
-        //todo Абиривиатуры возрастания и убывания:
+        //todo Абиривиатуры возростания и убывания:
         String ascending = "-a";
         String decrease = "-d";
 
@@ -52,32 +52,33 @@ public class Main {
         }
 
         //todo Создание сканера:
-        try (Scanner scanner = new Scanner(new FileInputStream(sourcePath))) {
+        try (Scanner scanner = new Scanner(new FileInputStream(sourcePath));
+             PrintWriter writer = new PrintWriter(outputPath)) {
             // todo 1 вариант: если передали числа
             if (isDataType) {
                 ArrayList<Integer> listOfNumbers = new ArrayList<>(); // Создаю пустой список интеджеров:
                 //todo Чтение файла из сканера
-                while (scanner.hasNextInt()) {
+                while (scanner.hasNext()) {
                     listOfNumbers.add(scanner.nextInt());
                 }
                 // todo 1 вариант: если сортировка по возростанию:
                 if (isAscending)/*если в командной строке передано "-a" , то сортировка по возростанию */ {
-
                     // todo сортируем по возростанию
                     sortAscending(listOfNumbers, new IntegerComparator()); //Сортировка
-                    System.out.println(listOfNumbers);
 
+                    String result = String.format("Цифры по возростанию = %s", listOfNumbers);
+                    System.out.println(result);
                 } else {
                     // todo 2 вариант: если сортировка по убыванию:
                     // todo сортируем по убыванию:
                     sortDecreasing(listOfNumbers, new IntegerComparator());
-                    System.out.println(listOfNumbers);
+
+                    String result = String.format("Цифры по убыванию = %s", listOfNumbers);
+                    System.out.println(result);
                 }
                 // todo Запись отсортированных данных в файл:
-                try (PrintWriter writer = new PrintWriter(outputPath)) {
-                    for (int k = 0; k < listOfNumbers.size(); ++k) {
-                        writer.println(listOfNumbers.get(k));
-                    }
+                for (Integer listOfNumber : listOfNumbers) {
+                    writer.println(listOfNumber);
                 }
 
             } else {
@@ -92,20 +93,26 @@ public class Main {
                 if (isAscending) {
                     // todo сортируем по возростанию
                     sortAscending(listOfLetters, new StringComparator()); //Сортировка
-                    System.out.println(listOfLetters);
+
+                    String result = String.format("Буквы по возростанию = %s", listOfLetters);
+                    System.out.println(result);
                 } else {
                     // todo 2 вариант: если сортировка по убыванию:
                     // todo сортируем по убыванию:
                     Sort.sortDecreasing(listOfLetters, new StringComparator()); //Сортировка
-                    System.out.println(listOfLetters);
+
+                    String result = String.format("Буквы по убыванию = %s", listOfLetters);
+                    System.out.println(result);
                 }
                 // todo Запись отсортированных данных в файл:
-                try (PrintWriter writer = new PrintWriter(outputPath)) {
-                    for (int k = 0; k < listOfLetters.size(); ++k) {
-                        writer.println(listOfLetters.get(k));
-                    }
+                for (String listOfLetter : listOfLetters) {
+                    writer.println(listOfLetter);
                 }
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Не найден файл in.txt");
+        } catch (InputMismatchException e) {
+            System.out.println("Файл содержит недопустимую информацию(поменяйте содержимое файла)!!!");
         }
     }
 }
