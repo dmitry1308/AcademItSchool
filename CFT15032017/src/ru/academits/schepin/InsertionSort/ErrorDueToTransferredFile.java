b4f7884f71aee7027cc1f32f233068fa989e9e30
dmitry1308 <bright1308@gmail.com>
2017-03-31 09:39:57 +0700

    Delete ErrorDueToTransferredFile.java

--- CFT15032017/src/ru/academits/schepin/InsertionSort/ErrorDueToTransferredFile.java
+++
@@ -1,7 +0,0 @@
-package ru.academits.schepin.InsertionSort;
-
-public class ErrorDueToTransferredFile extends RuntimeException {
-public ErrorDueToTransferredFile(String message){
-    super(message);
-}
-}
